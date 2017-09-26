5907d816336fe4baf7e7effd092043ed99d8f8b0
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-09-26 09:37:07 +0530

    Graph worked

---
+++ js/connect.js
@@ -0,0 +1,92 @@
+var intmpudata;
+var client = new Messaging.Client("broker.hivemq.com", 8000, "myclientid_" + parseInt(Math.random() * 100, 10));
+
+//Gets  called if the websocket/mqtt connection gets disconnected for any reason
+client.onConnectionLost = function (responseObject) {
+    //Depending on your scenario you could implement a reconnect logic here
+    alert("connection lost: " + responseObject.errorMessage);
+};
+
+//Connect Options
+var options = {
+    timeout: 3,
+    //Gets Called if the connection has sucessfully been established
+    onSuccess: function () {
+        alert("Connected");
+    },
+    //Gets Called if the connection could not be established
+    onFailure: function (message) {
+        alert("Connection failed: " + message.errorMessage);
+    }
+};
+
+//Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
+var publish = function (payload, topic, qos) {
+    //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
+    var message = new Messaging.Message(payload);
+    message.destinationName = topic;
+    message.qos = qos;
+    client.send(message);
+}
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+//Chart
+var canvas = document.getElementById('myChart');
+var data = {
+labels: [],
+datasets: [
+    {
+        label: "Deflection",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data:[],
+    }
+],
+};
+
+
+
+var option = {
+showLines: true
+};
+var myLineChart = Chart.Line(canvas,{
+data:data,
+options:option
+});
+
+
+//Gets called whenever you receive a message for your subscriptions
+client.onMessageArrived = function (message) {
+    //Do something with the push message you received
+     // $("#messages").append(message.payloadString +" ");
+     currentTime();
+           intmpudata = parseInt(message.payloadString);
+           var xaxisstring = m.toString();
+           var yaxisstring = h.toString();
+           var xylabel = yaxisstring + " " + xaxisstring;
+           addData(myLineChart,xylabel,intmpudata);
+document.getElementById("messages").innerHTML= message.payloadString;
+    client.unsubscribe("chart/topic");
+};
---
+++ js/setTime.js
@@ -0,0 +1,89 @@
+// Hello
+var milliseconds;;
+var h;
+var m;
+
+function currentTime(){
+
+	var d = new Date();
+	 h = d.getHours();
+	 m = d.getMinutes();
+	 milliseconds = h * 36e5 + m * 6e4;
+}
+function getCurrenttime(){
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	 milliseconds = h * 36e5 + m * 6e4;
+	document.getElementById("currentTime").innerHTML = milliseconds;
+}
+
+function canlcelAll(){
+	document.getElementById("demo").innerHTML = " ";
+	    document.getElementById("hours").innerHTML = " ";
+			  document.getElementById("min").innerHTML = " ";
+				  document.getElementById("milliscnd").innerHTML ="Enter Time Again";
+}
+
+document.getElementById("save").addEventListener("click", function(){
+
+		currentTime();
+	  var x = document.getElementById("timeonly").value;
+      publish(x,'full/tym',2);
+    // document.getElementById("demo").innerHTML = x;
+    
+    var hours = x.substring(0,2);
+    var intihours = parseInt(hours);
+    console.log(intihours);
+    var pubdiffhr = Math.abs(h-intihours);
+    var hrstrng = pubdiffhr.toString();
+    // document.getElementById("hours").innerHTML = hours;
+    
+    var min = x.substring(3,5);
+    var intiminute = parseInt(min);
+    var pubdiffmin = Math.abs(m-intiminute);
+    
+    var minstrng = pubdiffmin.toString();
+    var pubdiffstrng = hrstrng+":"+minstrng;
+    console.log(pubdiffstrng);
+    publish(pubdiffstrng,"last/tym",2);
+    // document.getElementById("min").innerHTML = min;
+    
+    
+    var notifyhour = Math.abs(h - intihours);
+    var notifyminute = Math.abs(m - intiminute);
+
+    alert("Alarm set for"+notifyhour+"hours and"+notifyminute+" Minutes");
+
+    var hrmilli = 36e5 * intihours;
+
+    var minmilli = 6e4 * parseInt(min);
+
+    var milli = hrmilli + minmilli ;
+
+    var diff = milliseconds - milli ;
+
+    var abs =  Math.abs(diff);
+
+    // document.getElementById("milliscnd").innerHTML = abs;
+    var strng = abs.toString();
+    //publish(strng,'presence',2);
+});
+
+document.getElementById("massage").addEventListener("click", function(){
+  var on = '1';
+  publish(on,'alarm/massage',2);
+  alert("Published");
+});
+
+document.getElementById("suscribe").addEventListener("click", function(){
+setInterval(function(){
+    client.subscribe('chart/topic', {qos: 2});
+    // alert("Subscribed")
+},10000)
+});
+
+document.getElementById("massagestop").addEventListener("click", function(){
+  var off = '0';
+  publish(off,'alarm/massage',2);
+});
